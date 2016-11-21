3ec366558ee0fa4b9c831268bc2c86d69e3f49be
Gareth Gomersall <ggomersall@gmail.com>
2016-11-21 21:53:02 +0000

    setup Link and components for About Example and Weather and linking to correct paths

--- app/app.jsx
+++ app/app.jsx
@@ -5,11 +5,16 @@ var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 // The below would be how you write it in ES5
 // var Route = require('react-router').Route;
 var Main = require('Main');
+var Weather = require('Weather');
+var About = require('About');
+var Examples = require('Examples');
 
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={Main}>
-
+      <Route path="about" component={About}/>
+      <Route path="examples" component={Examples}/>
+      <IndexRoute component={Weather}/>
     </Route>
   </Router>,
   document.getElementById('app')
