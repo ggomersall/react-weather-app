4a66a7f3357638817458d41caaad1185cf137bdb
Gareth Gomersall <ggomersall@gmail.com>
2016-11-27 19:59:04 +0000

    added api key to variable to make the link shorter

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -1,11 +1,12 @@
 var axios = require('axios');
 
-const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?units=metric&appid=617f2a3ae4dd1237c5e75d4351615b9a';
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?units=metric';
 
 module.exports = {
   getTemp: function(location) {
+    var apiKey = '617f2a3ae4dd1237c5e75d4351615b9a';
     var encodedLocation = encodeURIComponent(location);
-    var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+    var requestUrl = `${OPEN_WEATHER_MAP_URL}&appid=${apiKey}&q=${encodedLocation}`;
 
     return axios.get(requestUrl).then(function(res) {
       debugger;
