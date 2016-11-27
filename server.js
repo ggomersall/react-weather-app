fbcab680574aef85115b4a4709a3f714430eba75
Gareth Gomersall <ggomersall@gmail.com>
2016-11-27 20:45:21 +0000

    edited server.js and package.json to tell heroku hot to start app

--- server.js
+++ server.js
@@ -2,10 +2,22 @@ var express = require('express');
 
 // Create our app
 var app = express();
+// const means value can't be changed a const should be uppercase
+const PORT = process.env.PORT || 3001;
 
+// this is our middleware
+// this will redirect https to http
+app.use(function(req, res, next){
+  if(req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+
+})
 // express.static
 app.use(express.static('public'));
 
-app.listen(3001, function() {
-  console.log('Express server is up on port 3001')
+app.listen(PORT, function() {
+  console.log('Express server is up on port ' + PORT)
 });
