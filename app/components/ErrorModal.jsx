c68ee509394f3a7b06ff341d6b2c50b3bdfe7498
Gareth <ggomersall@gmail.com>
2016-12-07 16:22:55 +0000

    fixed the DOM manipulation error caused by foundation

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,4 +1,6 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
+var ReactDOMServer = require('react-dom/server');
 
 var ErrorModal = React.createClass({
     getDefaultProps: function() {
@@ -12,12 +14,8 @@ var ErrorModal = React.createClass({
         message: React.PropTypes.string.isRequired // this makes sure the message is required
     },
     componentDidMount: function() {
-        var modal = new Foundation.Reveal($('#error-modal'));
-        modal.open();
-    },
-    render: function() {
         var {title, message} = this.props;
-        return (
+        var modalMarkup = (
             <div id="error-modal" className="tiny reveal text-center" data-reveal="">
                 <h4>{title}</h4>
                 <p>{message}</p>
@@ -28,6 +26,21 @@ var ErrorModal = React.createClass({
                 </p>
             </div>
         );
+        // this stops the error of DOM mutation cause by foundation
+        var $modal = $(ReactDOMServer.renderToString(modalMarkup));
+        // this willl return the DOM node
+        $(ReactDOM.findDOMNode(this)).html($modal);
+
+        var modal = new Foundation.Reveal($('#error-modal'));
+        modal.open();   // this make changes to the DOM that react doesn't work well with
+                        // so we needed to move our render html into this componentDidMount function
+    },
+    render: function() {
+        return (
+            <div>
+            </div>
+        );
+        
     }
 });
 
